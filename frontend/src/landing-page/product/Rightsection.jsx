7cc6645167052b257551a1fbacc990f2fd27fb82
Mohd Adil <mohdadil.tech@gmail.com>
2025-07-20 11:54:34 -0400

    v8

--- frontend/src/landing-page/product/Rightsection.jsx
+++ frontend/src/landing-page/product/Rightsection.jsx
@@ -9,17 +9,17 @@ function Rightsection({ imgageUrl,
         <div className="container p-5 ">
       <div className="row p-3">
        
-        <div className="col-6 p-5 mt-5">
+        <div className="col-6 p-4 mt-5">
           <h1>{productName}</h1>
           <p>{productDescription}</p>
           <div>
             {" "}
             <a href={tryDemo}> Try demo</a>
-            <a href={learnMore} style={{marginLeft:'50px'}}>Learn more </a>
+            <a href={learnMore} style={{marginLeft:'45px'}}>Learn more </a>
           </div>
           
         </div>
-        <div className="col-6 p-5 ">
+        <div className="col-6 p-4 ">
           <img src={imgageUrl} alt="product"  />
         </div>
       </div>
